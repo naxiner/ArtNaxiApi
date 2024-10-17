e2e7f3abf37e8833c66df6b93bb51f94bffa7cb7
naxiner <kirienkodenis2002@gmail.com>
2024-10-17 20:39:17 +0300

    Add StableDiffusionRequest Model

---
+++ ArtNaxiApi/Models/SDRequest.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace ArtNaxiApi.Models
+{
+    public class SDRequest
+    {
+        [Required]
+        [JsonPropertyName("prompt")]
+        public string Prompt { get; set; } = string.Empty;
+
+        [JsonPropertyName("negative_prompt")]
+        public string? NegativePrompt { get; set; } = string.Empty;
+
+        [JsonPropertyName("styles")]
+        public List<string>? Styles { get; set; } = new List<string>();
+
+        [Required]
+        [JsonPropertyName("sampler_name")]
+        public string SamplerName { get; set; } = string.Empty;
+
+        [Required]
+        [JsonPropertyName("steps")]
+        public int Steps { get; set; }
+
+        [Required]
+        [JsonPropertyName("cfg_scale")]
+        public int CfgScale { get; set; }
+
+        [Required]
+        [JsonPropertyName("width")]
+        public int Width { get; set; }
+
+        [Required]
+        [JsonPropertyName("height")]
+        public int Height { get; set; }
+    }
+}

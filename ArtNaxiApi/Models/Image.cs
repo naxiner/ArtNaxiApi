bc9aff48a7cb047b738b0ccd68ec874e0639eae5
naxiner <kirienkodenis2002@gmail.com>
2024-10-17 21:13:08 +0300

    Add Image Model

---
+++ ArtNaxiApi/Models/Image.cs
@@ -0,0 +1,9 @@
+﻿namespace ArtNaxiApi.Models
+{
+    public class Image
+    {
+        public Guid Id { get; set; }
+        public string Url { get; set; }
+        public DateTime CreationTime { get; set; } = DateTime.Now;
+    }
+}

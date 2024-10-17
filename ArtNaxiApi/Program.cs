0dbafa6a5a117868f609ac87ecc85f7551197052
naxiner <kirienkodenis2002@gmail.com>
2024-10-17 22:39:57 +0300

    Add Swagger

--- ArtNaxiApi/Program.cs
+++ ArtNaxiApi/Program.cs
@@ -17,12 +17,22 @@ namespace ArtNaxiApi
 
             builder.Services.AddHttpClient();
             builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
 
             builder.Services.AddScoped<ISDService, SDService>();
 
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
+                    c.RoutePrefix = string.Empty;
+                });
+            }
 
             app.MapControllers();
 

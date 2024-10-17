152dbc8d52b847d60248fa4455f32f7ba1d52c00
naxiner <kirienkodenis2002@gmail.com>
2024-10-17 20:42:43 +0300

    Add Image Controller

---
+++ ArtNaxiApi/Controllers/ImageController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ArtNaxiApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ImageController : ControllerBase
+    {
+        public ImageController() { }
+
+
+    }
+}
--- ArtNaxiApi/Program.cs
+++ ArtNaxiApi/Program.cs
@@ -5,9 +5,14 @@ namespace ArtNaxiApi
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            
+            builder.Services.AddControllers();
+
             var app = builder.Build();
 
             app.MapGet("/", () => "Hello World!");
+
+            app.MapControllers();
 
             app.Run();
         }

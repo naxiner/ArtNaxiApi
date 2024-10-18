f1df4e9ca3657d8e8772a42618778eff857f75a6
naxiner <kirienkodenis2002@gmail.com>
2024-10-18 14:08:05 +0300

    Add CRUD operations to Image Controller

--- ArtNaxiApi/Controllers/ImageController.cs
+++ ArtNaxiApi/Controllers/ImageController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ArtNaxiApi.Models;
+using ArtNaxiApi.Repositories;
+using ArtNaxiApi.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ArtNaxiApi.Controllers
 {
@@ -6,8 +9,59 @@ namespace ArtNaxiApi.Controllers
     [Route("api/[controller]")]
     public class ImageController : ControllerBase
     {
-        public ImageController() { }
+        private readonly ISDService _sdService;
+        private readonly IImageRepository _imageRepository;
 
+        public ImageController(ISDService sdService, IImageRepository imageRepository)
+        {
+            _sdService = sdService;
+            _imageRepository = imageRepository;
+        }
 
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Image>>> GetAllImagesAsync()
+        {
+            var allImages = await _imageRepository.GetAllImagesAsync();
+            return Ok(allImages);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Image>> GetImageByIdAsync(Guid id)
+        {
+            var imageById = await _imageRepository.GetImageByIdAsync(id);
+
+            if (imageById == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(imageById);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Image>> GenerateImage(SDRequest sdRequest)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var imagePath = await _sdService.GenerateImageAsync(sdRequest);
+
+            return Ok(imagePath);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteImageById(Guid id)
+        {
+            var result = await _sdService.DeleteImageByIdAsync(id);
+
+            if (!result)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
     }
 }

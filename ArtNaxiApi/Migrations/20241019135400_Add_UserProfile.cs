d3cb9b48854427edf3af341a80941834c9c1cbf9
naxiner <kirienkodenis2002@gmail.com>
2024-10-19 17:29:05 +0300

    Add UserProfile Model and Migration

---
+++ ArtNaxiApi/Migrations/20241019135400_Add_UserProfile.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ArtNaxiApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_UserProfile : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserId",
+                table: "Images",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateTable(
+                name: "UserProfiles",
+                columns: table => new
+                {
+                    Guid = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ProfilePictureUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserProfiles", x => x.Guid);
+                    table.ForeignKey(
+                        name: "FK_UserProfiles_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Images_UserId",
+                table: "Images",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserProfiles_UserId",
+                table: "UserProfiles",
+                column: "UserId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Images_Users_UserId",
+                table: "Images",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Images_Users_UserId",
+                table: "Images");
+
+            migrationBuilder.DropTable(
+                name: "UserProfiles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Images_UserId",
+                table: "Images");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Images");
+        }
+    }
+}
--- ArtNaxiApi/Models/Image.cs
+++ ArtNaxiApi/Models/Image.cs
@@ -1,12 +1,13 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-
-namespace ArtNaxiApi.Models
+﻿namespace ArtNaxiApi.Models
 {
     public class Image
     {
         public Guid Id { get; set; }
         public string Url { get; set; }
         public DateTime CreationTime { get; set; } = DateTime.Now;
+
+        public Guid UserId { get; set; }
+        public User User { get; set; }
 
         public SDRequest Request { get; set; }
     }

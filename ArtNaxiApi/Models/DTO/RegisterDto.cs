8c0028d130abeb01cf3b81658f373883aa27b36e
naxiner <kirienkodenis2002@gmail.com>
2024-10-19 15:30:56 +0300

    Add validation to Login and Register DTO Models

--- ArtNaxiApi/Models/DTO/RegisterDto.cs
+++ ArtNaxiApi/Models/DTO/RegisterDto.cs
@@ -1,18 +1,22 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using ArtNaxiApi.Validation;
+using System.ComponentModel.DataAnnotations;
 
 namespace ArtNaxiApi.Models.DTO
 {
     public class RegistrDto
     {
-        [Required]
+        [Required(ErrorMessage = "Username is required.")]
+        [StringLength(50, ErrorMessage = "Username cannot be longer than 50 characters.")]
+        [RegularExpression(@"^[a-zA-Z0-9]+$", ErrorMessage = "Username must contain only alphanumeric characters.")]
         public string Username { get; set; }
 
-        [Required]
-        [EmailAddress]
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
+        [StringLength(50, ErrorMessage = "Email cannot be longer than 50 characters.")]
         public string Email { get; set; }
 
-        [Required]
-        [MinLength(6)]
+        [Required(ErrorMessage = "Password is required.")]
+        [PasswordComplexity]
         public string Password { get; set; }
     }
 }

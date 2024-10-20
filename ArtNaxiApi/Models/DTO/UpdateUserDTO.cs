047df3ed6112507ce3dfa7e95b9c5e85ea669300
naxiner <kirienkodenis2002@gmail.com>
2024-10-20 18:01:26 +0300

    Add UpdateUser DTO Model

---
+++ ArtNaxiApi/Models/DTO/UpdateUserDTO.cs
@@ -0,0 +1,22 @@
+﻿using ArtNaxiApi.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace ArtNaxiApi.Models.DTO
+{
+    public class UpdateUserDTO
+    {
+        [Required(ErrorMessage = "Username is required.")]
+        [StringLength(50, ErrorMessage = "Username cannot be longer than 50 characters.")]
+        [RegularExpression(@"^[a-zA-Z0-9]+$", ErrorMessage = "Username must contain only alphanumeric characters.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
+        [StringLength(50, ErrorMessage = "Email cannot be longer than 50 characters.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [PasswordComplexity]
+        public string Password { get; set; }
+    }
+}

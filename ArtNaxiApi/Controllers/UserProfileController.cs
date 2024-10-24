15616d9d6c64c70bee7dd08b9c31a9799add8f58
naxiner <kirienkodenis2002@gmail.com>
2024-10-24 20:11:36 +0300

    Add UserProfile Controller

---
+++ ArtNaxiApi/Controllers/UserProfileController.cs
@@ -0,0 +1,25 @@
+﻿using ArtNaxiApi.Models;
+using ArtNaxiApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArtNaxiApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserProfileController : ControllerBase
+    {
+        private readonly IUserProfileService _userProfileService;
+        public UserProfileController(IUserProfileService userProfileService)
+        {
+            _userProfileService = userProfileService;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<UserProfile> GetUserProfileAsync(Guid id)
+        {
+            var userProfile = await _userProfileService.GetProfileByUserIdAsync(id);
+
+            return userProfile;
+        }
+    }
+}

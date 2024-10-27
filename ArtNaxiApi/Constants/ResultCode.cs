631fa7bfe407223c0169924512006d2770d4614a
naxiner <kirienkodenis2002@gmail.com>
2024-10-27 16:05:54 +0200

    Add enum constants of Result Codes

---
+++ ArtNaxiApi/Constants/ResultCode.cs
@@ -0,0 +1,11 @@
+﻿namespace ArtNaxiApi.Constants
+{
+    public enum ResultCode
+    {
+        OK = 200,
+        NoContent = 204,
+        Unauthorized = 401,
+        Forbid = 403,
+        NotFound = 404
+    }
+}

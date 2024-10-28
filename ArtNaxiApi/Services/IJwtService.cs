5a7f0951ade5a07df5253ac42e23b2d93393bdb2
naxiner <kirienkodenis2002@gmail.com>
2024-10-28 18:46:28 +0200

    Update JWT Service

--- ArtNaxiApi/Services/IJwtService.cs
+++ ArtNaxiApi/Services/IJwtService.cs
@@ -1,9 +1,15 @@
 ﻿using ArtNaxiApi.Models;
+using System.Security.Claims;
 
 namespace ArtNaxiApi.Services
 {
     public interface IJwtService
     {
         string GenerateToken(User user);
+        string GenerateRefreshToken();
+        Task<bool> ValidateRefreshTokenAsync(Guid userId, string refreshToken);
+        ClaimsPrincipal GetPrincipalFromExpiredToken(string token);
+        void RemoveRefreshTokenFromCookie();
+        void SetRefreshTokenInCookie(string refreshToken);
     }
 }

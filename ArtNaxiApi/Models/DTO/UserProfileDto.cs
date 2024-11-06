fabacfa3670bdde70b7a0f24136a08d85d5450d3
naxiner <kirienkodenis2002@gmail.com>
2024-11-06 21:17:53 +0200

    Add Email to UserProfileDTO

--- ArtNaxiApi/Models/DTO/UserProfileDto.cs
+++ ArtNaxiApi/Models/DTO/UserProfileDto.cs
@@ -4,6 +4,7 @@
     {
         public Guid Id { get; set; }
         public string Username { get; set; }
+        public string Email { get; set; }
         public string ProfilePictureUrl { get; set; }
         public List<ImageDto> Images { get; set; }
     }

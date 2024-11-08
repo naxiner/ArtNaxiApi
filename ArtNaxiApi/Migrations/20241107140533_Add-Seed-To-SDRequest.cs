f0599c9297098251fecee26ab0725a5b4864e4cd
naxiner <kirienkodenis2002@gmail.com>
2024-11-08 16:09:35 +0200

    Add Seed to SDRequest and Migration

---
+++ ArtNaxiApi/Migrations/20241107140533_Add-Seed-To-SDRequest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ArtNaxiApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSeedToSDRequest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Seed",
+                table: "SDRequests",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Seed",
+                table: "SDRequests");
+        }
+    }
+}

4973c462c8c4e43d8242ddc73433c16b9852c5f2
naxiner <kirienkodenis2002@gmail.com>
2024-11-11 17:08:35 +0200

    Add IsBanned to User Model

---
+++ ArtNaxiApi/Migrations/20241111150538_Add_IsBanned_To_User.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ArtNaxiApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_IsBanned_To_User : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsBanned",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsBanned",
+                table: "Users");
+        }
+    }
+}

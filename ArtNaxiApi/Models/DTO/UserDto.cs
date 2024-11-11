4601af53eaba251aa8a02c91c6104a2369e92a7e
naxiner <kirienkodenis2002@gmail.com>
2024-11-11 23:32:25 +0200

    Add UserDTO Model

---
+++ ArtNaxiApi/Models/DTO/UserDto.cs
@@ -0,0 +1,13 @@
+﻿namespace ArtNaxiApi.Models.DTO
+{
+    public class UserDto
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public bool IsBanned { get; set; }
+    }
+}

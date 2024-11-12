efe5dc0fd1e082f132062a33b582a2ad1a0dc709
naxiner <kirienkodenis2002@gmail.com>
2024-11-12 15:15:34 +0200

    Add RoleValidator

---
+++ ArtNaxiApi/Validation/RoleValidator.cs
@@ -0,0 +1,19 @@
+﻿using ArtNaxiApi.Constants;
+
+namespace ArtNaxiApi.Validation
+{
+    public static class RoleValidator
+    {
+        private static readonly HashSet<string> ValidRoles = new HashSet<string>
+        {
+            Roles.Admin,
+            Roles.Moderator,
+            Roles.User
+        };
+
+        public static bool IsValidRole(string role)
+        {
+            return ValidRoles.Contains(role);
+        }
+    }
+}

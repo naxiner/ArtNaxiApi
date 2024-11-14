d4cf216fdf09bf4c30bd315332c2363f4752a219
naxiner <kirienkodenis2002@gmail.com>
2024-11-14 13:08:45 +0200

    Add base methods to StyleRepository

--- ArtNaxiApi/Repositories/IStyleRepository.cs
+++ ArtNaxiApi/Repositories/IStyleRepository.cs
@@ -5,6 +5,12 @@ namespace ArtNaxiApi.Repositories
 {
     public interface IStyleRepository
     {
+        Task<Style> GetStyleByIdAsync(Guid id);
         Task<Style> GetStyleByNameAsync(string styleName);
+        Task<IEnumerable<Style>> GetAllStylesAsync(int pageNumber, int pageSize);
+        Task<int> GetTotalStylesCountAsync();
+        Task AddStyleAsync(Style style);
+        Task UpdateStyleAsync(Style style);
+        Task DeleteStyleByIdAsync(Guid id);
     }
 }
--- ArtNaxiApi/Repositories/StyleRepository.cs
+++ ArtNaxiApi/Repositories/StyleRepository.cs
@@ -13,12 +13,54 @@ namespace ArtNaxiApi.Repositories
             _context = context;
         }
 
+        public async Task<Style> GetStyleByIdAsync(Guid id)
+        {
+            return await _context.Styles
+                .FirstOrDefaultAsync(s => s.Id == id);
+        }
+
         public async Task<Style> GetStyleByNameAsync(string styleName)
         {
-            var stylesByName = await _context.Styles
+            return await _context.Styles
                 .FirstOrDefaultAsync(s => s.Name == styleName);
+        }
 
-            return stylesByName;
+        public async Task<IEnumerable<Style>> GetAllStylesAsync(int pageNumber, int pageSize)
+        {
+            return await _context.Styles
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+        }
+
+        public async Task<int> GetTotalStylesCountAsync()
+        {
+            return await _context.Styles.CountAsync();
+        }
+
+        public async Task AddStyleAsync(Style style)
+        {
+            _context.Styles.Add(style);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateStyleAsync(Style style)
+        {
+            _context.Styles.Update(style);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteStyleByIdAsync(Guid id)
+        {
+            var style = await GetStyleByIdAsync(id);
+
+            if (style == null)
+            {
+                throw new KeyNotFoundException();
+            }
+
+            _context.Styles.Remove(style);
+            await _context.SaveChangesAsync();
         }
     }
 }

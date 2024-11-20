d6937c751f9e7c0d0a2bf18f9ea0f2d9e89f0e26
naxiner <kirienkodenis2002@gmail.com>
2024-11-20 14:29:42 +0200

    Add Like Model and Migration

---
+++ ArtNaxiApi/Migrations/20241120122204_Add_Like_Table.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ArtNaxiApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_Like_Table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Likes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    LikedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EntityId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    EntityType = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Likes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Likes_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Likes_EntityId_EntityType",
+                table: "Likes",
+                columns: new[] { "EntityId", "EntityType" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Likes_UserId",
+                table: "Likes",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Likes");
+        }
+    }
+}
---
+++ ArtNaxiApi/Models/Like.cs
@@ -0,0 +1,15 @@
+﻿namespace ArtNaxiApi.Models
+{
+    public class Like
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        
+        public Guid UserId{ get; set; }
+        public User User { get; set; }
+
+        public DateTime LikedAt { get; set; } = DateTime.UtcNow;
+
+        public Guid EntityId { get; set; }
+        public string EntityType { get; set; }
+    }
+}

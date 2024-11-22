722f345b3e88410453671a30d297410785eac64f
naxiner <kirienkodenis2002@gmail.com>
2024-11-22 17:15:23 +0200

    Add RegisterResponse Model

---
+++ ArtNaxiApi/Models/DTO/Responses/RegisterResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace ArtNaxiApi.Models.DTO.Responses
+{
+    public class RegisterResponse
+    {
+        public string Message { get; set; }
+        public string Token { get; set; }
+
+        public RegisterResponse(string message, string token)
+        {
+            Message = message;
+            Token = token;
+        }
+    }
+}

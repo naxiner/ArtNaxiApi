a45edfa670282fbd6ac1650519580ddec228f6ce
naxiner <kirienkodenis2002@gmail.com>
2024-11-22 16:39:48 +0200

    Add MessageResponse Model

---
+++ ArtNaxiApi/Models/DTO/Responses/MessageResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace ArtNaxiApi.Models.DTO.Responses
+{
+    public class MessageResponse
+    {
+        public string Message { get; set; }
+
+        public MessageResponse(string message)
+        {
+            Message = message;
+        }
+    }
+}

a2b858ec9e33a105dddb582ff5a794992c54a963
naxiner <kirienkodenis2002@gmail.com>
2024-11-22 16:40:25 +0200

    Add LoginResponse Model

---
+++ ArtNaxiApi/Models/DTO/Responses/LoginResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace ArtNaxiApi.Models.DTO.Responses
+{
+    public class LoginResponse
+    {
+        public string Token { get; set; }
+        public string RefreshToken { get; set; }
+
+        public LoginResponse(string token, string refreshToken)
+        {
+            Token = token;
+            RefreshToken = refreshToken;
+        }
+    }
+}
